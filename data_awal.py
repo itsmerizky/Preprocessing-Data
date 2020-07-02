992bed732a9815c0e01723b39ad65114babe2157
itsmerizky <59680016+itsmerizky@users.noreply.github.com>
2020-07-02 12:31:00 +0700

    Add files via upload

---
+++ data_awal.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+filepath_dict = {'data':   'data/data_tsv.txt'}
+
+df_list = []
+for source, filepath in filepath_dict.items():
+    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
+    df['source'] = source  # Add another column filled with the source name
+    df_list.append(df)
+
+df = pd.concat(df_list)
+#print(df.iloc[0])
